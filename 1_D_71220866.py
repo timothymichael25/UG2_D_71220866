1adba11cdc8dd3c36f0a0fc4883eb4001f3e7aef
timothymichael25 <117884601+timothymichael25@users.noreply.github.com>
2023-02-15 19:28:40 +0700

    Add files via upload

---
+++ 1_D_71220866.py
@@ -0,0 +1,19 @@
+print('============== MAKANAN ===============')
+print('1. Telur Bakar           : Rp. 7.000 ')
+print('2. Lele Terbang Mas Bhe  : Rp. 10.000 ')
+print('3. Es Coklat Lempu       : Rp. 5.000 ')
+print('4. Es Doger Langensari   : Rp. 13.000')
+
+print('============== PESANAN ===============')
+print(int(input('Telur Bakar    :')))
+print(int(input('Lele Terbang   : ')))
+print(int(input('Es Coklat      : ')))
+print(int(input('Es Doger       : ')))
+
+print('=============== TOTAL ==============')
+print('TOTAL TELUR BAKAR X 2    : Rp 14.000')
+print('LELE TERBANG X 1         : Rp 10.000')
+print('ES COKLAT X 3            : Rp 15.000')
+print('ES DOGER X 1             : Rp 13.000')
+
+print('Jumlah total yang harus dibayar adalah Rp 52.000')
---
+++ 2_D_71220866.py
@@ -0,0 +1,6 @@
+notlp = (int(input('Masukkan Nomor Telepon: ')))
+indosat = 'indosat'
+telkomsel = 'indosat'
+
+if indosat:
+    print('Anda menggunakan operator', indosat)
